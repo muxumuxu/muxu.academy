189a72f97e9603dd9ddf229fda413d7b60ebac1c
David Miotti <david@muxumuxu.com>
2017-11-24 22:13:16 +0100

    [-] Fix showing menu on the first tap (responsive only)

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -1,12 +1,6 @@
-// This is where it all goes :)
 function toggleMenu() {
-    var x = document.getElementById("menu");
-    var body = document.getElementsByTagName("BODY")[0];
-    if (x.className === "menu-items") {
-        x.className += " responsive";
-        body.className += " menu-open"
-    } else {
-        x.className = "menu-items";
-        body.className = ""
-    }
+  var body = document.getElementsByTagName('BODY')[0]
+  var x = document.getElementById('menu')
+  body.classList.toggle('menu-open')
+  x.classList.toggle('responsive')
 }
